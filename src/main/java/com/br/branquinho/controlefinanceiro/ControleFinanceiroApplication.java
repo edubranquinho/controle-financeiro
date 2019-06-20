e0c0d4bebc18b495e802ae8c7ec1038b20bd8cc6
edubranquinho@hotmail.com <edubranquinho@hotmail.com>
2019-06-19 21:57:31 -0300

    inicio do projeto

---
+++ src/main/java/com/br/branquinho/controlefinanceiro/ControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.br.branquinho.controlefinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleFinanceiroApplication.class, args);
+    }
+
+}
